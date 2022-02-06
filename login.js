6cfd6a44b05d36a3f6965c96f150bb3b414b4245
f78495162333 <mura.hung@frog-jump.com>
2022-02-06 17:46:07 +0800

    2

---
+++ login.js
@@ -0,0 +1,32 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
+
+    createApp(
+      {
+        data() {
+          return {
+            url: 'https://vue3-course-api.hexschool.io/v2',
+            path: 'mura_api',
+            user: {
+                username: 'mura.hung@frog-jump.com',
+                password: 'sam840429Z',
+            }
+          }
+        },
+        methods: {  
+          login() {
+            axios.post(`${this.url}/admin/signin`, this.user)
+              .then((res) => {
+                console.log(res.data.message);
+                const { expired, token } = res.data
+                console.log(expired,token);
+
+                //登入後需把 expired、token 存在 cookie 內
+                // document.cookie = `HaxToken=${token}; expires=${new Date(expired)};`;
+                // window.location.href = `./week2/week2.html`;
+              })
+              .catch((err) => console.log(err.data.message));
+          }
+          
+        }
+      }
+    ).mount('#app');
---
+++ week2/week2.js
@@ -0,0 +1,14 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
+//vue 起手式
+createApp(
+    {
+        data() {
+            return {
+                
+            }
+        },
+        methods:{
+
+        }
+    }
+).mount('#app');
